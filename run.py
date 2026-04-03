93262e564ccae8120d68502b2516cbdac96e82a5
mahdiiii04 <143207634+mahdiiii04@users.noreply.github.com>
2026-04-03 21:47:39 +0100

    Update run.py

--- run.py
+++ run.py
@@ -11,9 +11,9 @@ from algorithm import MAPPO
 def main():
     parser = argparse.ArgumentParser(description="Training script")
 
-    parser.add_argument('--n_epochs', type=int, default=1000)
+    parser.add_argument('--n_epochs', type=int, default=10)
     parser.add_argument('--name', type=str, default='mpe/simple_spread_v3')
-    parser.add_argument('--learning-rate', type=float, default=3e-4)
+    parser.add_argument('--learning_rate', type=float, default=3e-4)
     parser.add_argument('--n_agents', type=int, default=3)
     parser.add_argument('--max_steps', type=int, default=100)
     parser.add_argument('--agentic_keyword', type=str, default="agents")
@@ -26,6 +26,7 @@ def main():
     parser.add_argument('--frames_per_batch', type=int, default=6_000)
     parser.add_argument('--n_iters', type=int, default=5)
     parser.add_argument('--minibatch_size', type=int, default=128)
+    parser.add_argument('--entropy_eps', type=float, default=1e-4)
 
 
     args = parser.parse_args()
@@ -53,7 +54,13 @@ def main():
 
     check_env_specs(env)
 
-    mappo = MAPPO(env, env_device, minibatch_size=args.minibatch_size, agentic_keyword=args.agentic_keyword)
+    mappo = MAPPO(env, env_device, minibatch_size=args.minibatch_size,
+                  lr=args.learning_rate, clip_epsilon=args.clip_epsilon,
+                  gamma=args.gamma, lmbda=args.lmbda, entropy_eps=args.entropy_eps,
+                  max_grad_norm=args.max_grad_norm, share_params_policy=args.share_params_policy,
+                  share_params_critic=args.share_params_critic, frames_per_batch=args.frames_per_batch,
+                  n_iters=args.n_iters, agentic_keyword=args.agentic_keyword
+            )      
 
     result = mappo.train(num_epochs=args.n_epochs)
 
